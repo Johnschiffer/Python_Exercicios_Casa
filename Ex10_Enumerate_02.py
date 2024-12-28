87c5682a831734bb4b15fe51a0b3c516ab74466b
Johnschiffer <106044944+Johnschiffer@users.noreply.github.com>
2024-12-28 09:09:05 -0300

    28-12

--- Ex10_Enumerate_02.py
+++ Ex10_Enumerate_02.py
@@ -7,4 +7,4 @@ frutas = ['Maçã', 'Laranja', 'Morango', 'Limão']
 for indice, fruta in enumerate(frutas, 0):
     print(indice, fruta)
     if indice == 3:
-        print(f'{indice} {fruta} EM PROMOÇÃO')
+        print(f'{indice} {fruta} EM PROMOÇÃO!')
